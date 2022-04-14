70134c6659a581dda506d4a5531cd2e406fb87b1
Nilane Arruda Machado <nyllane16@gmail.com>
2022-04-14 18:15:35 -0300

    "dfAvgFailures"

--- src/main/scala/org/shape/com/test/Application.scala
+++ src/main/scala/org/shape/com/test/Application.scala
@@ -2,7 +2,7 @@ package org.shape.com.test
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, regexp_extract}
+import org.apache.spark.sql.functions.{avg, col, regexp_extract, regexp_replace}
 
 
 object Application {
@@ -31,18 +31,59 @@ object Application {
 
     val logLines = spark.read.text("data/equipment_failure_sensors.log")
 
-    val timestampExp = "\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})]"
-    val sensorExp = "\\w{6}\\[\\d+]:"
+    val equipments = spark.read.format("json")
+      .option("inferSchema", "true")
+      .option("multiline","true")
+      .load("data/equipment.json")
 
-    val logsDf = logLines.select(regexp_extract(col("value"),timestampExp,1).alias("timestamp"),
-                                 regexp_extract(col("value"),sensorExp,0).alias("error")
-    )
+    val timeExp = "\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})]"
+    val errorExp = "\\w{6}\\[\\d+]:"
+    val sensorExp = "\\d+"
+    val dateEXP = "\\d{4}-\\d{2}-\\d{2}"
 
-    logsDf.show(3,false)
+    val logsDf = logLines.select(regexp_extract(col("value"),timeExp,1).alias("time"),
+                                 regexp_extract(col("value"),errorExp,0).alias("error"))
+    .withColumn("sensor_id", regexp_extract(col("error"),sensorExp,0))
+    .withColumn("date", regexp_extract(col("time"),dateEXP, 0))
+    .drop("error")
+
+    val januaryFailures = logsDf.filter(col("date").between("2020-01-01", "2020-01-31"))
+
+  /* 1 – Total equipment failures that happened in January 2020?
+    11645
+  */
+
+   val dfEquipments = csvSensors.join(januaryFailures, Seq("sensor_id"))
+                                .join(equipments, Seq("equipment_id"))
+
+   val countEquipmentFailures = dfEquipments.groupBy("code").count().orderBy(col("count").desc)
+
+    /* 2 – Which equipment code had most failures?
+     Equipment with code E1AD07D4 had 1377 failures in January. Curiously the equipment with code 2C195700 was the only one with no failures at all.
+     */
+
+
+    val dfGroupOfEquipments = dfEquipments.groupBy("group_name","code").count()
+    val dfAvgFailures = dfGroupOfEquipments.groupBy("group_name").agg(avg("count").alias("avg_failures"))
+
+
+    dfAvgFailures.orderBy(col("avg_failures")).show(false)
 
 
 
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 }
