f81dd3398460b9819687f94297c5200cce4fa48c
Nilane Arruda Machado <nyllane16@gmail.com>
2022-04-18 15:30:27 -0300

    "some fixes"

--- src/main/scala/org/shape/com/test/Application.scala
+++ src/main/scala/org/shape/com/test/Application.scala
@@ -2,27 +2,23 @@ package org.shape.com.test
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{avg, col, regexp_extract, regexp_replace}
+import org.apache.spark.sql.functions.{avg, col, regexp_extract}
 
 
 object Application {
 
-  /*1 – Total equipment failures that happened?
-
-    2 – Which equipment code had most failures?
-
-    3 – Average amount of failures across equipment group, ordered by the number of failures in ascending order?
-  */
   def main(args:Array[String]) {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
+    // Building a SparkSession on the local machine
     val spark = SparkSession
       .builder
       .appName("SparkSQL")
       .master("local[*]")
       .getOrCreate()
 
+    // Loading the data
     val csvSensors = spark.read.format("csv")
       .option("header","true")
       .option("sep", ";")
@@ -36,10 +32,12 @@ object Application {
       .option("multiline","true")
       .load("data/equipment.json")
 
+    // Regular Expressions necessary to extract the data from the log archive
     val timeExp = "\\[(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})]"
     val errorExp = "\\w{6}\\[\\d+]:"
     val sensorExp = "\\d+"
     val dateEXP = "\\d{4}-\\d{2}-\\d{2}"
+
 
     val logsDf = logLines.select(regexp_extract(col("value"),timeExp,1).alias("time"),
                                  regexp_extract(col("value"),errorExp,0).alias("error"))
@@ -47,27 +45,25 @@ object Application {
     .withColumn("date", regexp_extract(col("time"),dateEXP, 0))
     .drop("error")
 
+    // Filtering out the data from January 2020
     val januaryFailures = logsDf.filter(col("date").between("2020-01-01", "2020-01-31"))
 
-  /* 1 – Total equipment failures that happened in January 2020?
-    11645
-  */
-
+    // Dataframe with all the data about the equipments
    val dfEquipments = csvSensors.join(januaryFailures, Seq("sensor_id"))
                                 .join(equipments, Seq("equipment_id"))
 
+    // Grouping the equipments by the code to count which one presents the most failures
    val countEquipmentFailures = dfEquipments.groupBy("code").count().orderBy(col("count").desc)
 
-    /* 2 – Which equipment code had most failures?
-     Equipment with code E1AD07D4 had 1377 failures in January. Curiously the equipment with code 2C195700 was the only one with no failures at all.
-     */
-
-
+   // Counting the errors by equipment group and code
     val dfGroupOfEquipments = dfEquipments.groupBy("group_name","code").count()
+
+    // Average of the errors by group of equipments, ordered by the number of failures
     val dfAvgFailures = dfGroupOfEquipments.groupBy("group_name").agg(avg("count").alias("avg_failures"))
+      .orderBy(col("avg_failures"))
 
 
-    dfAvgFailures.orderBy(col("avg_failures")).show(false)
+
 
 
 
